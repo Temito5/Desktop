64b4984c42711521f6ce3fa03d4270f46c640b78
Your Name <you@example.com>
2023-05-10 11:45:54 +0300

    first commit

---
+++ Temenuga/Cars-9D-№13/Cars-9D-№13/Car.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars_9D__13
+{
+    internal class Car
+    {
+		private string marka;
+
+		public string Marka
+		{
+			get { return marka; }
+			set { marka = value; }
+		}
+
+		private string model;
+
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+
+		private string  rNomer;
+
+		public string  RNomer
+		{
+			get { return rNomer; }
+			set { rNomer = value; }
+		}
+
+		private int godinaProizv;
+
+		public int GodinaProizv
+		{
+			get { return godinaProizv; }
+			set 
+			{
+				godinaProizv = value;
+				//if (value<1768 || value>2000)
+				//{
+				//	Console.WriteLine("I’m NOT Eco CAR");				
+
+    //            }
+				//else
+				//{
+				//	Console.WriteLine("ECO");
+                    
+				//}
+				 
+			}
+		}
+
+		private static byte counter;
+
+		public static byte Counter
+		{
+			get { return counter; }
+			set { counter = value; }
+		}
+		public void EcoIs()
+		{
+			if (godinaProizv>1768 && godinaProizv<2000)
+			{
+				Console.WriteLine("I’m NOT Eco CAR");
+			}
+			else
+			{
+				if (godinaProizv>2000)
+				{
+                     Console.WriteLine("ECO");
+				}
+				
+			}
+		}
+
+		public void MoveCar()
+		{
+			Console.WriteLine($"{this.Marka} {this.Model}: Brum-Brum !");
+		}
+
+		public void PrintMe()
+		{
+			Console.WriteLine($"{this.Marka} {this.Model} {this.RNomer} {this.GodinaProizv}");
+		}
+
+	
+		//public Car( string marka,string model ,string rNomer,int godinaProizv)
+		//{
+		//	Marka = marka;
+		//	Model = model;
+		//	RNomer = rNomer;
+		//	GodinaProizv=godinaProizv;
+		//	counter++;
+		//}
+	}
+}
---
+++ Temenuga/Cars-9D-№13/Cars-9D-№13/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars_9D__13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            Console.WriteLine("Iskash li na vkarash kola:");
+            string command=Console.ReadLine();
+            while (command == "Da")
+            {
+                Car car = new Car();
+
+                string marka = Console.ReadLine();
+                string model = Console.ReadLine();
+                string rNomer = Console.ReadLine();
+                byte godinaProizv = byte.Parse(Console.ReadLine());
+
+                car.Marka = marka;
+                car.Model = model;
+                car.RNomer = rNomer;
+                car.GodinaProizv = godinaProizv;
+
+                car.MoveCar();
+                car.PrintMe();
+                car.EcoIs();
+
+
+            }
+           
+
+
+            Console.WriteLine($"The cars/car is:{Car.Counter}");
+        }
+    }
+}
